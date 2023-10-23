444ff9b4ad36bc5ce31138a63a354bd64829cbfa
refrain62 <nao_h13@hotmail.com>
2023-10-23 19:24:05 +0900

    リンクとナビゲーション

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,24 @@
+'use client'
+
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+
 // `app/page.tsx` is the UI for the '/' URL
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+    const router = useRouter()
+
+    return (
+        <>
+            <h1>Hello, Next.js!</h1>
+            <Link href="/dashboard" scroll={false}>
+                Dashboard by LinkTag
+            </Link>
+            <br />
+            <button
+                type="button"
+                onClick={() => router.push('/dashboard')}>
+                    Dashboard by router.push
+                </button>
+        </>
+    )
 }
